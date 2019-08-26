951d1e41c080e2c4c51d8fde5cd0f2d3f4e3690a
Piotr <pgolczyk@blastlab.co>
2019-08-26 13:43:16 +0200

    Add weather service to Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,28 @@
 <template>
     <main class="app-content main">
         <p class="text-center">
-            Check the weather! Enter the ciny name, or <b>leave empty for geolocation</b>
+            Check the weather! Enter the city name, or <b>leave empty for geolocation</b>
         </p>
         <weather-form></weather-form>
-        <weather-box></weather-box>
+        <weather-box v-if="weather"></weather-box>
     </main>
 </template>
 
 <script>
-import WeatherForm from '@/components/WeatherForm'
-import WeatherBox from '@/components/WeatherBox'
-export default {
-    components: {
-        WeatherForm,
-        WeatherBox
+    import WeatherForm from '@/components/WeatherForm'
+    import WeatherBox from '@/components/WeatherBox'
+    import {
+        WeatherService
+    } from '@/services/WeatherService'
+    export default {
+        components: {
+            WeatherForm,
+            WeatherBox
+        },
+        mixins: [
+            WeatherService
+        ]
     }
-}
 </script>
 
 <style lang="scss">
