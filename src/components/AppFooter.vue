53f882cd79e1bb62bcf64dc1a2ef221d38592685
Piotr <pgolczyk@blastlab.co>
2019-08-26 13:44:23 +0200

    Add spinner icon for loading data

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,6 +1,14 @@
 <template>
     <footer class="app-footer">
-        Photo by Ibu Photographer on Unsplash
+        <p>
+            Photo by Ibu Photographer on Unsplash
+        </p>
+        <p>
+            <div>Icons made by <a href="https://www.flaticon.com/authors/dave-gandy" title="Dave Gandy">Dave Gandy</a>
+                from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a
+                    href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0"
+                    target="_blank">CC 3.0 BY</a></div>
+        </p>
     </footer>
 </template>
 
